60b2b051b0a2b266479f8c61f2092104b118c5b4
Jozef Sukovsky <sukovskyj@gmail.com>
2016-02-27 16:08:35 +0100

    reformatted code

--- djangocms_bootgrid/cms_plugins.py
+++ djangocms_bootgrid/cms_plugins.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
-from django.utils.translation import ugettext_lazy as _
+from decimal import Context
 
 from cms.models import CMSPlugin
-from cms.plugin_pool import plugin_pool
 from cms.plugin_base import CMSPluginBase
-
+from cms.plugin_pool import plugin_pool
+from django.utils.translation import ugettext_lazy as _
 from djangocms_bootgrid.forms import GridPluginForm, GridColumnPluginForm
 from djangocms_bootgrid.models import GridBootstrap, GridColumnBootstrap
-from decimal import Context
 
 class GridBootstrapPlugin(CMSPluginBase):
     model = GridBootstrap
@@ -17,33 +16,34 @@ class GridBootstrapPlugin(CMSPluginBase):
     allow_children = True
     child_classes = ['GridColumnBootstrapPlugin']
     form = GridPluginForm
-    
+
     def render(self, context, instance, placeholder):
         context.update({
-                        'grid': instance, 
-                        'placeholder': placeholder,
-                        })
+            'grid': instance,
+            'placeholder': placeholder,
+        })
         return context
-    
+
     def save_model(self, request, obj, form, change):
         response = super(
-                         GridBootstrapPlugin, 
-                         self).save_model(request, obj, form, change)
+            GridBootstrapPlugin,
+            self).save_model(request, obj, form, change)
         if not change:
             for x in xrange(int(form.cleaned_data['create'])):
                 col = GridColumnBootstrap(
-                                          parent=obj,
-                                          placeholder=obj.placeholder,
-                                          language=obj.language,
-                                          size_xs=form.cleaned_data.get('create_size_xs') or None,
-                                          size_sm=form.cleaned_data.get('create_size_sm') or None,
-                                          size_md=form.cleaned_data.get('create_size_md') or None,
-                                          size_lg=form.cleaned_data.get('create_size_lg') or None,
-                                          position=CMSPlugin.objects.filter(parent=obj).count(),
-                                          plugin_type=GridColumnBootstrapPlugin.__name__,
-                                          )
+                    parent=obj,
+                    placeholder=obj.placeholder,
+                    language=obj.language,
+                    size_xs=form.cleaned_data.get('create_size_xs') or None,
+                    size_sm=form.cleaned_data.get('create_size_sm') or None,
+                    size_md=form.cleaned_data.get('create_size_md') or None,
+                    size_lg=form.cleaned_data.get('create_size_lg') or None,
+                    position=CMSPlugin.objects.filter(parent=obj).count(),
+                    plugin_type=GridColumnBootstrapPlugin.__name__,
+                )
                 col.save()
             return response
+
 
 class GridColumnBootstrapPlugin(CMSPluginBase):
     model = GridColumnBootstrap
@@ -52,12 +52,12 @@ class GridColumnBootstrapPlugin(CMSPluginBase):
     render_template = 'djangocms_bootgrid/column.html'
     allow_children = True
     form = GridColumnPluginForm
-    
+
     def render(self, context, instance, placeholder):
         context.update({
-                        'column': instance,
-                        'placeholder': placeholder,
-                        })
+            'column': instance,
+            'placeholder': placeholder,
+        })
         return context
 
 plugin_pool.register_plugin(GridBootstrapPlugin)
--- djangocms_bootgrid/forms.py
+++ djangocms_bootgrid/forms.py
@@ -5,65 +5,76 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
 from .models import (
-                     GridBootstrap,
-                     BOOTSTRAP_GRID_CHOICES,
-                     GridColumnBootstrap
-                     )
+    GridBootstrap,
+    BOOTSTRAP_GRID_CHOICES,
+    GridColumnBootstrap
+)
 
 COLUMN_SIZE_CHOICES = [('', '----------')] + BOOTSTRAP_GRID_CHOICES
 NUM_COLUMNS = [
-               (i, '%s' % i) for i in range(0, 12)
-               ]
+    (i, '%s' % i) for i in range(0, 12)
+]
+
 
 class GridPluginForm(forms.ModelForm):
     create = forms.ChoiceField(
-                               choices=NUM_COLUMNS,
-                               label=_('Create Columns'),
-                               help_text=_('Create this number of columns inside')
-                               )
+        choices=NUM_COLUMNS,
+        label=_('Create Columns'),
+        help_text=_('Create this number of columns inside')
+    )
     create_size_xs = forms.ChoiceField(
-                                       choices=COLUMN_SIZE_CHOICES,
-                                       label=_('Column size (xs)'),
-                                       help_text=('Width of created columns. You can still change the width of the column afterwards.'),
-                                       required=False
-                                       )
+        choices=COLUMN_SIZE_CHOICES,
+        label=_('Column size (xs)'),
+        help_text=(
+            'Width of created columns. You can still change the \
+width of the column afterwards.'),
+        required=False
+    )
     create_size_sm = forms.ChoiceField(
-                                       choices=COLUMN_SIZE_CHOICES,
-                                       label=_('Column size (sm)'),
-                                       help_text=('Width of created columns. You can still change the width of the column afterwards.'),
-                                       required=False
-                                       )
+        choices=COLUMN_SIZE_CHOICES,
+        label=_('Column size (sm)'),
+        help_text=(
+            'Width of created columns. You can still change the width of \
+the column afterwards.'),
+        required=False
+    )
     create_size_md = forms.ChoiceField(
-                                       choices=COLUMN_SIZE_CHOICES,
-                                       label=_('Column size (md)'),
-                                       help_text=('Width of created columns. You can still change the width of the column afterwards.'),
-                                       required=False
-                                       )
+        choices=COLUMN_SIZE_CHOICES,
+        label=_('Column size (md)'),
+        help_text=(
+            'Width of created columns. You can still change the width of the \
+column afterwards.'),
+        required=False
+    )
     create_size_lg = forms.ChoiceField(
-                                       choices=COLUMN_SIZE_CHOICES,
-                                       label=_('Column size (lg)'),
-                                       help_text=('Width of created columns. You can still change the width of the column afterwards.'),
-                                       required=False
-                                       )
-    
+        choices=COLUMN_SIZE_CHOICES,
+        label=_('Column size (lg)'),
+        help_text=(
+            'Width of created columns. You can still change the width of the \
+column afterwards.'),
+        required=False
+    )
+
     class Meta:
         model = GridBootstrap
-        exclude = ('page', 'position', 'placeholder', 'language', 'plugin_type')
+        exclude = (
+            'page', 'position', 'placeholder', 'language', 'plugin_type')
+
 
 class GridColumnPluginForm(forms.ModelForm):
-    
+
     class Meta:
         model = GridColumnBootstrap
         fields = '__all__'
-    
+
     def clean(self):
         data = super(GridColumnPluginForm, self).clean()
         sizes = [
-                 data['size_xs'],
-                 data['size_sm'],
-                 data['size_md'],
-                 data['size_lg'],
-                 ]
+            data['size_xs'],
+            data['size_sm'],
+            data['size_md'],
+            data['size_lg'],
+        ]
         if not any(sizes):
             raise ValidationError(_('Please provide at least one size'))
         return data
